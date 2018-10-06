e3bee080c200ea2860081d79f907f50c1bcd0359
alex cebrian <a.cebrian@gmail.com>
2018-10-06 13:46:24 -0700

    added sort solution

--- prolog/src/day2_sort.pl
+++ prolog/src/day2_sort.pl
@@ -1,2 +1,8 @@
-sort([], []).
-/* ??? */
+xsort([], []).
+xsort([Head|Tail], Sorted) :- 
+	sort(Tail, STail),
+	insert(Head, STail, Sorted).
+
+insert(X, [], [X]).
+insert(X, [H|Tail], [X,H|Tail]) :- X =< H.
+insert(X, [H|Tail], [H|Tail2]) :- X > H, insert(X, Tail, Tail2).
