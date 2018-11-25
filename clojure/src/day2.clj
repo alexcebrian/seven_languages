1827c24132a2ed0c2ace5bad439647647db6a1de
alex cebrian <a.cebrian@gmail.com>
2018-11-25 13:03:41 -0800

    day 2

---
+++ clojure/src/day2.clj
@@ -0,0 +1,12 @@
+
+(defmacro unless [test body else]
+	(list 'if (list 'not test) body else))
+
+(defprotocol Transport
+	(drive [d])
+	(recharge [d]))
+
+(defrecord Car [fuel mpg]
+	Transport
+	(drive [_] (Car. (- fuel (/ 1 mpg)) mpg))
+	(recharge [_] (Car. (+ fuel 1) mpg)))
