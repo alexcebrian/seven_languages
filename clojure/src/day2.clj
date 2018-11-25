141ab704d0ad2d1461399a27a908d85092b64970
alex cebrian <a.cebrian@gmail.com>
2018-11-25 13:28:50 -0800

    optinal else

--- clojure/src/day2.clj
+++ clojure/src/day2.clj
@@ -1,6 +1,7 @@
 
-(defmacro unless [test body else]
-	(list 'if (list 'not test) body else))
+(defmacro unless 
+	([test body] (list 'if (list 'not test) body))
+	([test body else] (list 'if (list 'not test) body else)))
 
 (defprotocol Transport
 	(drive [d])
